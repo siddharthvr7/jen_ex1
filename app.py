1202d6303b15b1b8839c7e12c92f45563cc49349
siddharthvr7 <siddharthvr7@gmail.com>
2026-09-24 02:07:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-def multiply(a, b):
-    return a * b
+def greet(name):
+    message = f"Hello, {name}!"
+    print(message)
 
 
-def divide(a, b):
-    return a / b
+if __name__ == "__main__":
+    greet("Jenkins")
